9d64c0bd35f69f11939be0d1565cba083a7306ae
csdavids519 <csdavds@proton.me>
2024-10-29 00:09:57 +0100

    Modify settings.py for new database and installed apps

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
+    'reservations',
 ]
 
 MIDDLEWARE = [
