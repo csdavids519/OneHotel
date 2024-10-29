59ba9a881bbfe9d1e72319939a37a0521fa269e9
csdavids519 <csdavds@proton.me>
2024-10-29 17:03:27 +0100

    Add models ER diagram image, update reservations models

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # installed apps
+    'django_extensions',
+
+    # project apps
     'home',
     'reservations',
 ]
@@ -79,13 +84,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
+
 # Create your models here.
+class Bookings(models.Model):
+    booking_code = models.CharField(max_length=5, unique=True)
+
+    def __str__(self):
+        return self.booking_code
+    
+
+class Rooms(models.Model):
+    ROOM_TYPE = (
+        ('1','Basic'),
+        ('2','Premium'),
+        ('3','Executive'),
+    )
+
+    BED_TYPE = (
+        ('1','Single Bed'),
+        ('2','Double Bed'),
+        ('3','Queen Bed'),
+    )
+
+    room_number = models.IntegerField(unique=True)
+    room_type = models.CharField(choices= ROOM_TYPE)
+    floor = models.IntegerField()
+    bed_type = models.CharField(choices= BED_TYPE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    booking_code = models.ForeignKey(
+        Bookings, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.room_number)
+
+
+class Customer(models.Model):
+    customer_id = models.IntegerField(unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    booking_code = models.ForeignKey(
+        Bookings, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.last_name
+    
+    def __str__(self):
+        return self.customer_id
+
+
