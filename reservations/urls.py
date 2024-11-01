916337e47fbe2c1eaeccd3333cedc92625a3233c
csdavids519 <csdavds@proton.me>
2024-11-01 18:19:30 +0100

    Display room options on home page - in progress

--- reservations/urls.py
+++ reservations/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.RoomsList.as_view(), name='RoomsList'),
+    path('roomdetail/',views.room_detail, name="room_detail")
 ]
--- reservations/views.py
+++ reservations/views.py
@@ -8,3 +8,12 @@ class RoomsList(generic.ListView):
     queryset = Room.objects.filter(room_type=1)
     template_name = "index.html" 
     paginate_by = 6
+
+
+def room_detail(request):
+    room_number = Room.objects.all()
+   
+    context = {'room_number':room_number}
+
+    return render(request, "room_detail.html", context)
+    
