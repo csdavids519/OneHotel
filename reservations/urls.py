34c28dcefeb09ea777cffd859180939236184766
csdavids519 <csdavds@proton.me>
2024-11-06 16:55:01 +0100

    Add template logic for user login

--- reservations/urls.py
+++ reservations/urls.py
@@ -2,7 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    # path('', views.RoomsList.as_view(), name='RoomsList'),
-    # path('roomdetail/',views.room_detail, name="room_detail"),
     path('', views.FilterList, name="FilterList"),
 ]
