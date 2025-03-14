e0bb84dab8c954f59fc1962bc013d823015738a5
csdavids519 <csdavds@proton.me>
2025-03-14 18:28:10 +0100

    hide old key from production - sk has been updated

--- config/settings.py
+++ config/settings.py
@@ -26,11 +26,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ehdraiu^ptt&!u5oveh&lnyi$f7ctz)+rq(b_omy$bg3$f_m-2'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
