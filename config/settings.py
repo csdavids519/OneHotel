37ce625d2af29c1d592c2c0bd031213e3adcfa50
csdavids519 <csdavds@proton.me>
2025-03-23 16:18:06 +0100

    create new security key added to enev file

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
